cbd296871745aacec49990fcb4aca9e3437f4cbb
williambjork <william.bjork@gobil.se>
2022-08-22 10:04:25 +0200

    more

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,10 +3,11 @@ import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth ({
     providers: [
-        GoogleProviders ({
+        GoogleProvider ({
             clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
     secret: process.env.JWT_SECRET
 });
+
