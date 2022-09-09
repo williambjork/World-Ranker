c599ae83157e8c92e3c8164986cb006977e4f53a
williambjork <william.bjork@gobil.se>
2022-09-09 11:09:39 +0200

    bugfix

--- components/Checkbox.js
+++ components/Checkbox.js
@@ -7,6 +7,7 @@ function Checkbox(props) {
         type="checkbox"
         name="type[game]"
         class="w-5 h-5 border-gray-300 rounded"
+        onClick={props.handleCheckbox}
         />
 
         <label  class="ml-3 text-sm font-medium"> {props.title} </label>
