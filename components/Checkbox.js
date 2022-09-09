8d19e01ddc69a1946858116e6171063a3a1731cc
williambjork <william.bjork@gobil.se>
2022-09-09 11:24:57 +0200

    added boolean checkbox state

--- components/Checkbox.js
+++ components/Checkbox.js
@@ -5,12 +5,13 @@ function Checkbox(props) {
         <input
         id="game"
         type="checkbox"
-        name="type[game]"
-        class="w-5 h-5 border-gray-300 rounded"
-        onClick={props.handleCheckbox}
+        name={props.title}
+        className="w-5 h-5 border-gray-300 rounded"
+        onChange={props.handleOnChange}
+        
         />
 
-        <label  class="ml-3 text-sm font-medium"> {props.title} </label>
+        <label  className="ml-3 text-sm font-medium"> {props.title} </label>
 
   </div>
   )
