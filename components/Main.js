e413141b91bc83c2c439d4e8d4ca47e981898901
williambjork <william.bjork@gobil.se>
2022-09-09 11:24:57 +0200

    added boolean checkbox state

--- components/Main.js
+++ components/Main.js
@@ -1,29 +1,37 @@
 import { useState } from "react"
 import Checkbox from "./Checkbox"
 
-function handleCheckbox() {
-    console.log("click")
-}
+
+
 
 function Main() {
 
+    const handleOnChange = () => {
+        setIsChecked(!isChecked);
+    }
+    
+
     const [price, setPrice] = useState();
+    const [isChecked, setIsChecked] = useState(false);
+
 
   return (
-    <a class="relative block p-8 border border-gray-100 shadow-xl rounded-xl" href="">
+    <a className="relative block p-8 border border-gray-100 shadow-xl rounded-xl" href="">
  
 
-  <div class="mt-4 text-gray-500 sm:pr-8">
+  <div className="mt-4 text-gray-500 sm:pr-8">
    
      
 
-    <h5 class="mt-4 text-xl font-bold text-gray-900">Pizza Time!</h5>
+    <h5 className="mt-4 text-xl font-bold text-gray-900">Pizza Time!</h5>
 
-    <p class="hidden mt-2 text-sm sm:block">
+    <p className="hidden mt-2 text-sm sm:block">
       Your pizza is: {price}
     </p>
 
-    <Checkbox title="Pepperoni" cost={1} handleCheckbox={handleCheckbox}/>
+    <p>Check box is {isChecked ? "checked" : "no checked"}</p>
+
+    <Checkbox title="Pepperoni" cost={1} handleOnChange={handleOnChange} isChecked={isChecked}/>
     <Checkbox title="Cheese" cost={3}/>
     <Checkbox title="Bacon" cost={7}/>
     <Checkbox title="Kebab" cost={10}/>
