b33bcd2848e5b05c311482c742ff0247805622c1
williambjork <william.bjork@gobil.se>
2022-09-12 14:02:37 +0200

    installed heroicons, styling

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,13 +1,41 @@
+import { HomeIcon } from '@heroicons/react/24/solid'
+import { BookmarkIcon } from '@heroicons/react/24/solid'
+
 function Sidebar() {
   return (
-    <div className="flex align-items">
-        <a>Game Pass</a>
-        <a>My Library</a>
-        <a>Cloud Gaming</a>
-        <a>Community</a>
-        <a>Store</a>
+    <div className="h-screen overflow-y-scroll border-r border-gray-900 p-5 
+    text-xs lg:text-sm text-white scrollbar-hide
+    sm:max-w-[15rem] lg:max-w-[18rem]  pb-36 left-3 bg-black">
+        <div className="space-y-4">
 
-        <p>Installerad</p>
+        <div className="hover:bg-gray-700 px-4 py-2 rounded">
+            <button className="flex items-center space-x-2  ">
+                <HomeIcon className=""  />
+                <h3 className="">Game Pass</h3>
+            </button>
+        </div>
+
+            <button className="flex items-center space-x-2 ">
+                <BookmarkIcon />
+            <h3>My Library</h3>
+            </button>
+
+            <button className="flex items-center space-x-2 ">
+            <h3>Cloud Gaming</h3>
+            </button>
+
+            <button className="flex items-center space-x-2 ">
+            <h3>Community</h3>
+            </button>
+
+            <button className="flex items-center space-x-2 ">
+            <h3>Store</h3>
+            </button>
+
+            <button className="flex items-center space-x-2 ">
+            <p>Installerad</p>
+            </button>
+        </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,21 +8,19 @@ import Sidebar from "../components/Sidebar"
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <Main />
-        
+      <main className="">
+      {/*  <Main /> */}
+
         <Sidebar />
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        
-      </footer>
+   
     </div>
   )
 }
