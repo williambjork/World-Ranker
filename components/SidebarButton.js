50a74cce2ad100423fc41aa8ab4a9541f0690ac7
williambjork <william.bjork@gobil.se>
2022-09-13 12:40:13 +0200

    added searchbar

--- components/SidebarButton.js
+++ components/SidebarButton.js
@@ -1,7 +1,7 @@
 
 function SidebarButton(props) {
   return (
-    <div className="hover:bg-gray-700 px-4 py-2 rounded">
+    <div className="hover:bg-gray-700 hover:neumo1 px-4 py-2 rounded">
             <button className="flex items-center space-x-2  ">
                 
                 
--- components/User.js
+++ components/User.js
@@ -10,15 +10,15 @@ function User() {
         
         <div className="justify-content ">
             
-            <p className=" px-4 py-2 ">Delicious Donut  </p>
-            <p className="pl-4 bg-white text-black text-xs">PC GAMES</p>
+            <p className=" px-4 py-2 hocer:cursor-pointer">Delicious Donut  </p>
+           {/*} <p className="pl-4 bg-white text-black text-xs">PC GAMES</p> */}
         </div>
         
             
             
       
 
-        <BellIcon className="w-7 h-7 tr-3 hover:bg-gray-700 rounded" />
+        <BellIcon className="w-7 h-7 mt-4 hover:bg-gray-700 rounded" />
 
         </div>
         
