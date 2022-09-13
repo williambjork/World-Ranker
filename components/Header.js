a8cd95fb5d6bfc4e1e9dcaa98298c82db2162472
williambjork <william.bjork@gobil.se>
2022-09-13 13:20:59 +0200

    refactored searchbar/backbutton into components

--- components/Header.js
+++ components/Header.js
@@ -1,41 +1,14 @@
-import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+
+import Searchbar from "./Searchbar"
+import BackButton from "./BackButton"
 
 function Header() {
   return (
     <div className="flex">
-      <div className="pl-3 pt-3">
-        <button className="flex items-center bg-opacity-40 bg-gray-400 hover:bg-gray-300 hover:opacity-60 rounded-md p-2">
-          <ChevronLeftIcon className="w-7 h-7" />
-        </button>
-      </div>
-
-    <div className="flex justify-center pt-3">
-      <div class="relativeflex">
-        <label class="sr-only" for="email">
-          {" "}
-          Search{" "}
-        </label>
-
-        <input
-          class="w-50 py-3 pl-3 pr-12 ml-9 text-sm bg-gray-700  border-none rounded"
-          id="email"
-          type="email"
-          placeholder="Search"
-        />
-
-          <span class="absolute text-gray-400 pointer-events-none  top-1/2 right-4">
-            <svg
-              class="w-5 h-5"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-            
-            </svg>
-          </span>
-        </div>
-      </div>
+      
+      <BackButton />
+      <Searchbar />
+      
     </div>
   );
 }
