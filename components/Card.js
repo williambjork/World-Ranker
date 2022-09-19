0c6270c8da6dc8066abbc67890dc60f0d3b83ead
williambjork <william.bjork@gobil.se>
2022-09-19 14:11:15 +0200

    fixed card layout css

--- components/Card.js
+++ components/Card.js
@@ -16,12 +16,13 @@ function Card({maintitle, subtitle, metacritic, image}) {
   
 
   return (
-    <div>
+    <div className=" ">
       <a
         href=""
-        className="  relative block overflow-hidden bg-center bg-no-repeat bg-cover  rounded-xl"
+        className=" relative block overflow-hidden bg-center rounded-xl"
         
       >
+        
         <span className={`absolute z-10 inline-flex items-center px-3 py-1 text-xs font-semibold text-white bg-black rounded-full  right-4 top-4`}>
           {metacritic}
           <svg
@@ -33,10 +34,11 @@ function Card({maintitle, subtitle, metacritic, image}) {
             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
           </svg>
         </span>
-        <Thumbnail image={image} />
+        <Thumbnail  image={image} />
 
-        <div className="relative p-8 pt-40 text-white bg-black bg-opacity-40">
-          <h5 className="text-xl font-bold truncate">{maintitle}</h5>
+        <div className="relative  p-4 text-white bg-black bg-opacity-40">
+        
+          <h5 className="text-xl font-bold  truncate">{maintitle}</h5>
 
           <p className="text-sm truncate">{subtitle}</p>
         </div>
