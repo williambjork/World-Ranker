6170b1d38ffce3f4349beb29ea4252e6d7962ae4
williambjork <william.bjork@gobil.se>
2022-09-21 15:22:29 +0200

    added card animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,23 @@ import Carousel from "../components/Carousel";
 import Card from "../components/Card";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { motion } from "framer-motion"
 
 const Home: NextPage = () => {
   const [games, setGames] = useState([]);
 
-
+   
 
   async function fetchGames() {
     const url =
-      "https://api.rawg.io/api/games?key=f6d4a95732b6497e929238e5994121e6&dates=2019-09-01,2019-09-30&platforms=18,1,7";
+      "https://api.rawg.io/api/games?key=f6d4a95732b6497e929238e5994121e6&dates=2020-09-01,2020-09-30&platforms=18,1,7";
     const response = await fetch(url);
     const data = await response.json();
     setGames(data.results);
     console.log(games);
+    
+    
+    
   } 
 
 
@@ -42,7 +46,22 @@ const Home: NextPage = () => {
           </div>
           <div className="container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 ">
             {games.map((game) => (
-              <div>
+              <motion.div
+              initial={{
+                x: -200,
+                opacity: 0,
+                scale: 0.5,
+              }}
+              animate={{
+                x: 0,
+                opacity: 1,
+                scale: 1,
+              }}
+              transition={{
+                ease: "easeOut",
+                duration: 1,
+              }}
+                >
                 <Card
                   metacritic={game.metacritic}
                   key={game.id}
@@ -51,7 +70,7 @@ const Home: NextPage = () => {
                   image={game?.background_image}
                 />
                
-              </div>
+              </motion.div>
             ))}
           </div>
         </div>
