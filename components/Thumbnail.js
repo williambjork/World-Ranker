ab71b667207f7cda95a4a3411dcb293b7bfce7a8
williambjork <william.bjork@gobil.se>
2022-09-21 14:29:50 +0200

    installed framer motion

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -2,8 +2,10 @@ import Image from "next/image"
 
 function Thumbnail(props) {
   return (
-    <div>
+    <div className="hover:blur-sm relative overflow-hidden w-full h-full">
+      <div>
         <Image layout="responsive" src={props.image} height={100} width={200}/>
+        </div>
     </div>
   )
 }
