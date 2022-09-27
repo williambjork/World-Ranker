1752336f6b68dfd590df66405877180a8bed0480
williambjork <william.bjork@gobil.se>
2022-09-27 13:23:08 +0200

    added links to pages

--- components/SidebarButton.js
+++ components/SidebarButton.js
@@ -1,8 +1,12 @@
+import { useRouter } from "next/router"
 
 function SidebarButton(props) {
+
+  const router = useRouter();
+
   return (
     <div className="hover:bg-gray-700 hover:neumo1 px-4 py-2 rounded">
-            <button className="flex items-center space-x-2  ">
+            <button onClick={() => {router.push(props.url)}} className="flex items-center space-x-2  ">
                 
                 
                     {props.icon} 
