8774dbbc6c1a9c566cd44a77e45885c4b573d241
williambjork <william.bjork@gobil.se>
2022-09-27 11:28:59 +0200

    added page

---
+++ pages/topGames.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
+import { useEffect, useState } from "react";
+import Header from "../components/Header";
+import Carousel from "../components/Carousel";
+import Card from "../components/Card";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
+import { fetchTopGames } from "../lib/loadGames"
+
+type Props = {}
+
+function topGames({games}: Props) {
+    return (
+        <div>
+          <Head>
+            <title>Create Next App</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+    
+          <main className=" flex">
+            <Sidebar /> 
+    
+            <div className="bgmesh h-screen w-screen overflow-hidden overflow-y-scroll scrollbar-hide">
+              <Header />
+             
+    
+              <div className="flex justify-center text-3xl text-white pt-9 pb-2">
+                <h2>Top Games</h2>
+              </div>
+              <div className="container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 ">
+                {games.map((game) => (
+                  <motion.div
+                  initial={{
+                    x: -200,
+                    opacity: 0,
+                    scale: 0.5,
+                  }}
+                  animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1,
+                  }}
+                  transition={{
+                    type: 'spring',
+                    duration: 1,
+                  }}
+                    >
+                    <Card
+                      metacritic={game.metacritic}
+                      key={game.id}
+                      maintitle={game.name}
+                      subtitle={game.released}
+                      image={game?.background_image}
+                    />
+                   
+                  </motion.div>
+                ))}
+              </div>
+            </div>
+          </main>
+        </div>
+      );
+    };
+
+    export async function getStaticProps() {
+        const data = await fetchTopGames()
+        
+        
+        return {
+          props: { games : data.results },
+        }
+      }
+
+export default topGames
