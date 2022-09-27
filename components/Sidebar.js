d8a66ca4c6afe86cf744732e5dbc3e8066a326dc
williambjork <william.bjork@gobil.se>
2022-09-27 14:51:14 +0200

    bugfix

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -21,7 +21,7 @@ function Sidebar(props) {
         <User />
 
         
-        <SidebarButton icon={<HomeIcon className="w-6 h-6"/>} title="Home"/>
+        <SidebarButton icon={<HomeIcon className="w-6 h-6"/>} url="/" title="Home"/>
         <SidebarButton icon={<BookmarkIcon className="w-6 h-6"/>} url="/topGames" title="Top Games"/>
         <SidebarButton icon={<CloudIcon className="w-6 h-6"/>} title="Upcoming"/> 
         <SidebarButton icon={<UserIcon className="w-6 h-6" />} title="Community"/>
---
+++ pages/game.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function game({}: Props) {
+  return (
+    <div>game</div>
+  )
+}
+
+export default game
