fbf27eceed8605b845967a877ce9de803fb3e700
williambjork <william.bjork@gobil.se>
2022-09-28 13:38:08 +0200

    image optimization

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,7 +4,7 @@ function Thumbnail(props) {
   return (
     <div className=" relative overflow-hidden w-full h-full">
       <div>
-        <Image layout="responsive" objectFit="cover" src={props.image} height={100} width={200}/>
+        <Image layout="responsive" priority="true" objectFit="cover" src={props.image} height={100} width={200}/>
         </div>
     </div>
   )
