d466dea7b2d799b33db93c19bb3c090ecb5ac0e5
williambjork <william.bjork@gobil.se>
2022-09-30 15:35:18 +0200

    styling

--- components/Card.js
+++ components/Card.js
@@ -44,7 +44,7 @@ function Card({maintitle, subtitle, metacritic, image}) {
         <Thumbnail  image={image} />
         </div>
 
-        <div className="relative  p-4 text-white bg-black bg-opacity-40">
+        <div className="relative  p-4 text-white bg-gray-600 bg-opacity-30">
         
           <h5 className="text-xl font-bold  truncate">{maintitle}</h5>
 
