2a8dba2682c6e6baa05589f5e20aa8ce2a9c44f5
williambjork <william.bjork@gobil.se>
2022-09-30 14:40:13 +0200

    home styling

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import CardList from '../components/CardList'
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
 
 type Props = {}
 
 function home({}: Props) {
   return (
-    <div>home</div>
+    <main className=" flex">
+            <Sidebar /> 
+    
+            <div className="bgmesh h-screen w-screen overflow-hidden overflow-y-scroll scrollbar-hide">
+              <Header />
+             
+    
+              <div className="flex justify-center font-eurostile text-3xl text-white pt-9 pb-2">
+                
+              </div>
+              
+              
+
+          
+              
+            </div>
+          </main>
   )
 }
 
