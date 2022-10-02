ad4050bde0b82269067f0230050e68ff1aac3c8b
williambjork <william.bjork@gobil.se>
2022-10-02 13:02:30 +0200

    added image gradient

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -2,9 +2,11 @@ import Image from "next/image"
 
 function Thumbnail(props) {
   return (
-    <div className=" relative overflow-hidden w-full h-full">
+    <div className="bg-gray-600 bg-opacity-30 relative overflow-hidden w-full h-full">
       <div>
-        <Image layout="responsive" priority="true" objectFit="cover" src={props.image} height={100} width={200}/>
+        <Image 
+        className="fade-image" 
+        layout="responsive" priority="true" objectFit="cover" src={props.image} height={100} width={200}/>
         </div>
     </div>
   )
