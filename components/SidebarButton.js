56a01e06eb41332b7df9231070cce1c49db571db
williambjork <william.bjork@gobil.se>
2022-10-02 18:09:43 +0200

    font styling

--- components/SidebarButton.js
+++ components/SidebarButton.js
@@ -6,7 +6,7 @@ function SidebarButton(props) {
 
   return (
     <div className="hover:bg-gray-700 hover:neumo1 px-4 py-2 rounded">
-            <button onClick={() => {router.push(props.url)}} className="flex items-center space-x-2 font-oswald ">
+            <button onClick={() => {router.push(props.url)}} className="flex items-center space-x-2 font-semibold font-oswald ">
                 
                 
                     {props.icon} 
